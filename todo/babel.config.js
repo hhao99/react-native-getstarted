7e31cf2d31a9b73734cf9c829fa93f72e13603da
hhao99 <hhao99@gmail.com>
2024-06-24 01:22:46 -0500

    todo with nativewind integration

---
+++ todo/babel.config.js
@@ -0,0 +1,9 @@
+const { plugins } = require("./postcss.config");
+
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['nativewind/babel'],
+  };
+};
