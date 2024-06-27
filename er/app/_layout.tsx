545778c7005ccea01248d81c1def6154aedaaa42
hhao99 <hhao99@gmail.com>
2024-06-27 12:01:11 -0500

    expo router v0

---
+++ er/app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import Page from './index'
+function RootLayout() {
+    return(
+        <View>
+            <Page />
+        </View>
+    )
+}
+
+export default RootLayout
---
+++ er/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Page() {
+    return (
+        <View>
+            <Text>Hello Expo Router!</Text>
+        </View>
+    )
+}
+
+export default Page
